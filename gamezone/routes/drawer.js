e336cea9c02a664c936fbdf0d6b1e35894263643
lawrence <lawrenceo@activedgetechnologies.com>
2020-09-05 11:34:00 +0100

    fixed react-navigation-drawer bug

--- gamezone/routes/drawer.js
+++ gamezone/routes/drawer.js
@@ -1,18 +1,18 @@
-import { createDrawerNavigator } from 'react-navigation-drawer';
-import { createAppContainer } from 'react-navigation';
+import { createDrawerNavigator } from "react-navigation-drawer";
+import { createAppContainer } from "react-navigation";
 
 // stacks
-import HomeStack from './homeStack';
-import AboutStack from './aboutStack';
+import HomeStack from "./homeStack";
+import AboutStack from "./aboutStack";
 
 // drawer navigation options
 const RootDrawerNavigator = createDrawerNavigator({
-  Home: {
-    screen: HomeStack,
-  },
-  About: {
-    screen: AboutStack,
-  },
+	Home  : {
+		screen : HomeStack,
+	},
+	About : {
+		screen : AboutStack,
+	},
 });
 
 export default createAppContainer(RootDrawerNavigator);
