4ca3c1c917bcc2c1a26ca8644032b484c71ca02a
lawrence <lawrenceo@activedgetechnologies.com>
2020-09-05 11:39:08 +0100

    refactored app.js

--- gamezone/App.js
+++ gamezone/App.js
@@ -1,27 +1,20 @@
-import React, { useState } from 'react';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo';
-import Navigator from './routes/drawer';
+import React, { useState } from "react";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+import Navigator from "./routes/drawer";
 
-const getFonts = () => Font.loadAsync({
-  'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
-  'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
-});
+const getFonts = () =>
+	Font.loadAsync({
+		"nunito-regular" : require("./assets/fonts/Nunito-Regular.ttf"),
+		"nunito-bold"    : require("./assets/fonts/Nunito-Bold.ttf"),
+	});
 
-export default function App() {
-  const [fontsLoaded, setFontsLoaded] = useState(false);
+const App = () => {
+	const [ fontsLoaded, setFontsLoaded ] = useState(false);
 
-  if (fontsLoaded) {
-    return (
-      <Navigator />
-    );
-  } else {
-    return (
-      <AppLoading 
-        startAsync={getFonts} 
-        onFinish={() => setFontsLoaded(true)} 
-      />
-    )
-  }
+	if (fontsLoaded) return <Navigator />; // if fonts are loaded show app
 
-}
+	return <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />; // if fonts are not loaded, load them.
+};
+
+export default App;
